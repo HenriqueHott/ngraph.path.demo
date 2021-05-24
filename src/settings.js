3f07968f45b07ff3032b5174e1f01258452b6c8b
Lucas <lucas123carvalho@icloud.com>
2021-05-23 21:55:29 -0300

    Added Belo Horizonte map

--- src/settings.js
+++ src/settings.js
@@ -10,32 +10,11 @@ function getSettings(qs) {
   };
 }
 
-
 function getAvailableGraphs(qs) {
   let graphs = [{
-    value: 'amsterdam-roads',
-    name: 'Amsterdam (76K edges, 1.1 MB)'
-  }, {
-    value: 'seattle-roads',
-    name: 'Seattle (173K edges, 2.4 MB)'
-  }, {
-    value: 'rome-roads',
-    name: 'Rome (258K edges, 3.8 MB)'
-  }, {
-    value: 'delhi-roads',
-    name: 'Delhi (280K edges, 3.9 MB)'
-  }, {
-    value: 'moscow-roads',
-    name: 'Moscow (451K edges, 6.5 MB)'
-  }, {
-    value: 'USA-road-d.NY',
-    name: 'New York (730K edges, 7.6 MB)'
-  },
-  // Commenting this out, as on mobile devices it may crash the browser.
-  //  {
-  //   value: 'tokyo-roads',
-  //   name: 'Tokyo (879K edges, 12.3 MB)'
-  // }
+    value: 'bh',
+    name: 'bh (120K vértices, 1.7 MB)'
+  }
 ];
 
   return {
